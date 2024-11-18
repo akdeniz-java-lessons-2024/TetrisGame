757b2264628b14f3494b1dde93a94f159373d141
fatihorucc <fatihorucc16@gmail.com>
2024-11-18 19:26:36 +0300

    Update JShape.java

--- src/tetrisblocks/JShape.java
+++ src/tetrisblocks/JShape.java
@@ -5,8 +5,13 @@ import tetris.TetrisBlock;
 public class JShape extends TetrisBlock {
     public JShape() {
         super(new int[][] { { 0, 1 },
-                { 0, 1 },
-                { 1, 1 }
+                {1, 1},
+                {1, 1}
         });
+    }
+}
+@Override
+    public void spawn(int gridWidth) {
+    super.spawn(gridWidth);
     }
 }
