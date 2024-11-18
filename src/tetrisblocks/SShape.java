ffecdb4c2773da598cd32d5d12e62c64db316279
fatihorucc <fatihorucc16@gmail.com>
2024-11-18 19:30:06 +0300

    Update SShape.java

--- src/tetrisblocks/SShape.java
+++ src/tetrisblocks/SShape.java
@@ -4,8 +4,10 @@ import tetris.TetrisBlock;
 
 public class SShape extends TetrisBlock {
     public SShape() {
-        // TODO: Add SShape Here
         super(new int[][] {
+
+            {0,1,1},
+            {1,1,0},
         });
     }
 }
