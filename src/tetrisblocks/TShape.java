fd721d7bc81d0923557a43dc1239b8c8c44234ef
Aarda07 <bilginaliarda@gmail.com>
2024-11-20 16:13:56 +0300

    Update TShape.java

--- src/tetrisblocks/TShape.java
+++ src/tetrisblocks/TShape.java
@@ -6,6 +6,9 @@ public class TShape extends TetrisBlock {
     public TShape() {
         // TODO: Add TShape Here
         super(new int[][] {
+            { 1, 1, 1 },
+            { 0, 1, 0 },
+            { 0, 1, 0 }
         });
     }
 }
