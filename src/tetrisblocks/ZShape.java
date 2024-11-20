811fe10bb0e4a03f53dff02830de1adc81de6cef
Aarda07 <bilginaliarda@gmail.com>
2024-11-20 16:14:11 +0300

    Update ZShape.java

--- src/tetrisblocks/ZShape.java
+++ src/tetrisblocks/ZShape.java
@@ -6,6 +6,8 @@ public class ZShape extends TetrisBlock {
     public ZShape() {
         // TODO: Add ZShape Here
         super(new int[][] {
+            { 1, 1, 0 },
+            { 0, 1, 1 }
         });
     }
 }
