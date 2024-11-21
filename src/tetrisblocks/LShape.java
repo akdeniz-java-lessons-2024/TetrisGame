e831eade5676a7694f6bb53a6b473885b9f79f78
yakuphann <yakuphan0209@gmail.com>
2024-11-21 22:10:14 +0300

    YakuphanYar

---
+++ src/tetrisblocks/LShape.java
@@ -0,0 +1,26 @@
+package tetrisblocks;
+
+import tetris.TetrisBlock;
+
+public class LShape extends TetrisBlock {
+    public LShape() {
+        super(new int[][] {
+                {1, 0},
+                {1, 0},
+                {1, 1}
+        });
+    }
+    @Override
+    public void rotate() {
+        super.rotate();
+
+        if (this.getWidth() == 1) {
+            this.setX(this.getX() + 1);
+            this.setY(this.getY() - 1);
+        } else {
+            this.setX(this.getX() - 1);
+            this.setY(this.getY() + 1);
+        }
+
+    }
+}
