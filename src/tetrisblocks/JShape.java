92f3e021ceb3a550d7d4ec7da4fff20298514580
Ahmet S. Baltacı <20210805053@ogr.akdeniz.edu.tr>
2024-12-10 19:56:28 +0300

    Update JShape.java

--- src/tetrisblocks/JShape.java
+++ src/tetrisblocks/JShape.java
@@ -4,9 +4,9 @@ import tetris.TetrisBlock;
 
 public class JShape extends TetrisBlock {
     public JShape() {
-        super(new int[][] { { 0, 1 },
-                { 0, 1 },
-                { 1, 1 }
+        super(new int[][]{
+            {1, 0, 0},
+            {1, 1, 1}
         });
     }
 }
